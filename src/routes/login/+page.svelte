80276611704a914036fce2fbb58dcd4b43a9ee3a
Fabien Bézagu <fbezagu@tiron.fr>
2025-07-07 09:22:05 +0200

    Permets de saisir le login et le mot de passe sur la page de connexion

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -2,20 +2,109 @@
 	import { goto } from '$app/navigation';
 	import { onMount } from 'svelte';
 
+	let login = $state('');
+	let motDePasse = $state('');
+	let erreurConnexion: string | null = $state(null);
+
 	onMount(() => {
 		localStorage.removeItem('authToken');
 	});
 
-	const login = async () => {
-		const reponse = await fetch('/login', { method: 'POST' });
-		let data = await reponse.json();
-		localStorage.setItem('authToken', data.token);
-		goto('/');
+	const connecte = async () => {
+		const reponse = await fetch('/login', {
+			method: 'POST',
+			body: JSON.stringify({ login, motDePasse })
+		});
+		if (reponse.status === 200) {
+			const data = await reponse.json();
+			localStorage.setItem('authToken', data.token);
+			goto('/');
+		} else {
+			erreurConnexion = (await reponse.json()).message;
+		}
 	};
 </script>
 
-<form onsubmit={login}>
-	<button>
-		Login
-	</button>
-</form>
+<main>
+	<form onsubmit={connecte}>
+		<label>
+			<span>Login</span>
+			<input type="text" bind:value={login} />
+		</label>
+		<label>
+			<span>Mot de passe</span>
+			<input type="password" bind:value={motDePasse} />
+		</label>
+		<button> Se connecter</button>
+		{#if erreurConnexion}
+			<p class="erreur">{erreurConnexion}</p>
+		{/if}
+	</form>
+</main>
+
+<style lang="scss">
+  :global(body) {
+    font-family: Lato, sans-serif;
+    font-size: 16px;
+    color: #222;
+    padding: 0;
+    margin: 0;
+  }
+
+  main {
+    margin: 40px 24px;
+  }
+
+  form,
+  label {
+    display: flex;
+    flex-direction: column;
+  }
+
+  form {
+    gap: 16px;
+  }
+
+  label {
+    gap: 4px;
+  }
+
+  input[type='text'],
+  input[type='password'] {
+    padding: 8px;
+    font-size: 16px;
+    border-radius: 4px;
+    border: 1px solid #ddd;
+
+    &:hover {
+      outline: 2px solid #2461ae;
+    }
+  }
+
+  button {
+    display: flex;
+    gap: 16px;
+    justify-content: center;
+    border: none;
+    background-color: #2461ae;
+    color: white;
+    padding: 12px;
+    font-weight: bold;
+    cursor: pointer;
+    font-size: 18px;
+    border-radius: 4px;
+
+    &:hover:not(&[disabled]) {
+      opacity: 90%;
+    }
+
+    &[disabled] {
+      background-color: #888;
+      cursor: not-allowed;
+    }
+  }
+
+  .erreur {
+		color: red;
+	}
+</style>
--- src/routes/login/+server.ts
+++ src/routes/login/+server.ts
@@ -1,8 +1,14 @@
-import { json, type RequestHandler } from '@sveltejs/kit';
+import { error, json, type RequestHandler } from '@sveltejs/kit';
 import jwt from 'jsonwebtoken';
 import { env } from '$env/dynamic/private';
 
-export const POST: RequestHandler = () => {
-	const token = jwt.sign({ id: 'fabien' }, env.SECRET_JWT, { expiresIn: '5m' });
-	return json({ token });
+export const POST: RequestHandler = async ({ request }) => {
+	const donneesConnexion = (await request.json()) as { login: string; motDePasse: string };
+	const { login } = donneesConnexion;
+
+	if (login === 'fabien') {
+		const token = jwt.sign({ id: login }, env.SECRET_JWT, { expiresIn: '5m' });
+		return json({ token });
+	}
+	throw error(401, 'Utilisateur ou mot de passe invalide');
 };
